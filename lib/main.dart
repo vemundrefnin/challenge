bad1d8f1d4a3c4331487b1dfdae349b036e5a027
Vemund Refnin <vemund.refnin@gmail.com>
2020-03-28 15:58:45 +0100

    reset

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,79 @@
-import 'package:challenge/router.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  FluroRouter.setupRouter();
-  runApp(MyApp());
+  runApp(Challenge());
 }
 
-class MyApp extends StatelessWidget {
-  final FirebaseUser user=null;
+class Challenge extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<FirebaseUser>(
-        future: FirebaseAuth.instance.currentUser(),
-        builder: (BuildContext context, AsyncSnapshot<FirebaseUser> snapshot) {
-            // FirebaseUser user = snapshot.data; // this is your user instance
-          if (snapshot.hasData) {
-            /// is because there is user already logged
-            return MaterialApp(
-                title: 'Challenge',
-                initialRoute: 'home',
-                onGenerateRoute: FluroRouter.router.generator
-                //  theme: appTheme(),
-                );
-          }
+    return MaterialApp(
+      title: 'Challenge',
+      theme: ThemeData(primarySwatch: Colors.blue,),
+      home: HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
 
-          /// other way there is no user logged.
-          return MaterialApp(
-              title: 'Challenge',
-              initialRoute: 'login',
-              onGenerateRoute: FluroRouter.router.generator
-              );
-        });
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
